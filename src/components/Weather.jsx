b9ca9ae297badcbe382c8f5622bfa2c85685a58c
maheeen-khan <maheeenkhan4@gmail.com>
2025-01-24 17:48:42 +0500

    updated

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import './Weather.css'
-import {SearchOutlined} from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons'
 const Weather = () => {
-  return (
-    <>
-    <h1>Weather App</h1>
-    <div className="inp-wrap">
-    <input type="text" placeholder='Search' title='Enter City name' className='inp'/>
-    <div className="search">
-    <SearchOutlined />
-    </div>
-    </div>
-    </>
-  )
+    return (
+        <>
+            <h1 className='head'>Weather App</h1>
+            <br />
+            <div className="inp-wrap">
+                <input type="text" placeholder='Search' title='Enter City name' className='inp' />
+                <div className="search">
+                    <SearchOutlined />
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Weather
